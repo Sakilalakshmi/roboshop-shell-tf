f515172a1fc1947a3f339d2d9525067f658a8505
Sakila <sakilalakshmi95@gmail.com>
2023-10-04 10:52:09 +0530

    user data

--- cart.sh
+++ cart.sh
@@ -69,7 +69,7 @@ npm install &>> $LOGFILE
 
 VALIDATE $? "Installing dependencies"
 
-cp /home/centos/roboshop-shell-tf/cart.service /etc/systemd/system/cart.service &>> $LOGFILE
+cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service &>> $LOGFILE
 
 VALIDATE $? "copying cart.service"
 
